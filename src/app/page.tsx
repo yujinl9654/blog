bf482a95c3d8b7e0f1f1c4bb31d750962cb22fbd
yujin.lee <yujinl9654@gmail.com>
2024-07-28 17:50:26 +0900

    [feat] featured posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import FeaturedPosts from '@/components/FeaturedPosts';
 import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      {/* @ts-expect-error Server Component */}
+      <FeaturedPosts />
     </>
   );
 }
---
+++ src/components/FeaturedPosts.tsx
@@ -0,0 +1,15 @@
+import { getFeaturedPosts } from '@/service/posts';
+import PostsGrid from './PostsGrid';
+
+export default async function FeaturedPosts() {
+  // 1. 모든 포스트 데이터 읽어와야 함
+  const posts = await getFeaturedPosts();
+
+  // 2. 모든 포스트 데이터 보여줌
+  return (
+    <section>
+      <h2 className="text-2xl font-bold my-2">Featured Posts</h2>
+      <PostsGrid posts={posts} />
+    </section>
+  );
+}
